1df2e26ef8fd826c4fc68cdb1f529726222a7ea2
pradeep <luckkey005@gmail.com>
2020-01-01 18:41:41 +0530

    individual popup gallery added

---
+++ js/check.js
@@ -0,0 +1,226 @@
+//crocodile gallery
+
+$('#croc').magnificPopup({
+    items: [
+      {
+        src: 'images/crocodile.jpg',
+        title:'crocodile front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/crocodile-2.jpg',
+        title:'crocodile back',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/crocodile-w.jpg',
+        title:'crocodile wired front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/crocodile-w2.jpg',
+        title:'crocodile wired back',
+        class:'img-fluid'
+      }          
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//plane gallery
+$('#plane').magnificPopup({
+    items: [
+      {
+        src: 'images/plane.jpg',
+        title:'plane ',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/plane-w.jpg',
+        title:'plane wired',
+        class:'img-fluid'
+      },
+             
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//borodon gallery
+$('#borodon').magnificPopup({
+    items: [
+      {
+        src: 'images/borodon.jpg',
+        title:'borodon front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/borodon-2.jpg',
+        title:'borodon back',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/borodon-w.jpg',
+        title:'borodon wired front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/borodon-w2.jpg',
+        title:'borodon wired back',
+        class:'img-fluid'
+      }          
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//sofa-chair gallery
+$('#sofa-chair').magnificPopup({
+    items: [
+      {
+        src: 'images/sofa-chair.jpg',
+        title:'sofa-chair front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/sofa-chair-2.jpg',
+        title:'sofa-chair back',
+        class:'img-fluid'
+      },
+             
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//chakra gallery
+$('#chakra').magnificPopup({
+    items: [
+      {
+        src: 'images/chakra.jpg',
+        title:'chakra front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/chakra-2.jpg',
+        title:'chakra back',
+        class:'img-fluid'
+      },
+             
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//plain-chair gallery
+$('#plain-chair').magnificPopup({
+    items: [
+      {
+        src: 'images/plain-chair.jpg',
+        title:'plain-chair front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/plain-chair-2.jpg',
+        title:'plain-chair back',
+        class:'img-fluid'
+      },
+             
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//office-chair gallery
+$('#office-chair').magnificPopup({
+    items: [
+      {
+        src: 'images/office-chair.jpg',
+        title:'office-chair front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/office-chair-2.jpg',
+        title:'office-chair back',
+        class:'img-fluid'
+      },
+             
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//chair-pattern gallery
+$('#chair-pattern').magnificPopup({
+    items: [
+      {
+        src: 'images/chair-pattern.jpg',
+        title:'chair-pattern front',
+        class:'img-fluid'
+      },
+      {
+        src: 'images/chair-pattern-2.jpg',
+        title:'office-chair back',
+        class:'img-fluid'
+      },
+             
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
