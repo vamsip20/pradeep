5ec055faea90ec0d8f007afdc9edcacabcabd716
pradeep <luckkey005@gmail.com>
2020-01-02 10:22:12 +0530

    3d models embeded

--- js/check.js
+++ js/check.js
@@ -109,6 +109,11 @@ $('#sofa-chair').magnificPopup({
         title:'sofa-chair back',
         class:'img-fluid'
       },
+      {
+        src: 'https://sketchfab.com/models/8d1d4ae936e74d68a9d6d6090edc754b/embed',
+        title:'sofa-chair 3D model',
+        type:'iframe'
+      },
              
     ],
     gallery: {
@@ -134,6 +139,11 @@ $('#chakra').magnificPopup({
         src: 'images/chakra-2.jpg',
         title:'chakra back',
         class:'img-fluid'
+      },
+      {
+        src: 'https://sketchfab.com/models/8d97184484b94f85bd416da14b3e5903/embed',
+        title:'chakra 3D model',
+        type:'iframe'
       },
              
     ],
@@ -161,6 +171,11 @@ $('#plain-chair').magnificPopup({
         title:'plain-chair back',
         class:'img-fluid'
       },
+      {
+        src: 'https://sketchfab.com/models/15dec510c0cf48ac93c367e23f13904b/embed',
+        title:'plain-chair 3D model',
+        type:'iframe'
+      },
              
     ],
     gallery: {
@@ -187,6 +202,11 @@ $('#office-chair').magnificPopup({
         title:'office-chair back',
         class:'img-fluid'
       },
+      {
+        src: 'https://sketchfab.com/models/308433e100b74b26907e01849ddd78bc/embed',
+        title:'office-chair 3d view',
+        type:'iframe'
+      }
              
     ],
     gallery: {
@@ -210,7 +230,33 @@ $('#chair-pattern').magnificPopup({
       },
       {
         src: 'images/chair-pattern-2.jpg',
-        title:'office-chair back',
+        title:'chair-pattern back',
+        class:'img-fluid'
+      },
+      {
+        src: 'https://sketchfab.com/models/4090e225f3184ee8aac892e800c2f783/embed',
+        title:'chair-pattern 3D model',
+        type:'iframe'
+      },
+             
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//hand gallery
+$('#hand').magnificPopup({
+    items: [
+      { 
+        src: 'images/hand.jpg',
+        title:'hand',
         class:'img-fluid'
       },
              
@@ -224,6 +270,126 @@ $('#chair-pattern').magnificPopup({
       verticalFit: true, // Fits image in area vertically
       tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
     }
+});
+
+//cage gallery
+$('#cage').magnificPopup({
+    items: [
+      { 
+        src: 'images/cage.jpg',
+        title:'cage',
+        class:'img-fluid'
+      },
+      { 
+        src: 'https://sketchfab.com/models/92e221f9a45845d8959738ad4f671538/embed',
+        title:'cage 3D model',
+        type:'iframe'
+      },
+             
+    ],
+    gallery: {
+      enabled: true 
+    },
+    type: 'image', // this is default type
+    image: {
+      titleSrc: 'title', //gives title for image
+      verticalFit: true, // Fits image in area vertically
+      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    }
+});
+
+//home gallery
+$('#home-t').magnificPopup({
+  items: [
+    { 
+      src: 'images/home-t.jpg',
+      title:'home',
+      class:'img-fluid'
+    },
+           
+  ],
+  gallery: {
+    enabled: true 
+  },
+  type: 'image', // this is default type
+  image: {
+    titleSrc: 'title', //gives title for image
+    verticalFit: true, // Fits image in area vertically
+    tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+  }
+});
+
+//cola gallery
+$('#cola').magnificPopup({
+  items: [
+    { 
+      src: 'images/cola.jpg',
+      title:'cola',
+      class:'img-fluid'
+    },
+           
+  ],
+  gallery: {
+    enabled: true 
+  },
+  type: 'image', // this is default type
+  image: {
+    titleSrc: 'title', //gives title for image
+    verticalFit: true, // Fits image in area vertically
+    tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+  }
+});
+
+//detol gallery
+$('#detol').magnificPopup({
+  items: [
+    { 
+      src: 'images/detol.jpg',
+      title:'detol',
+      class:'img-fluid'
+    },
+    { 
+      src: 'https://sketchfab.com/models/170acc5be8f24f06b7db94c3f07d4775/embed',
+      title:'detol 3D model',
+      type:'iframe'
+    },
+           
+  ],
+  gallery: {
+    enabled: true 
+  },
+  type: 'image', // this is default type
+  image: {
+    titleSrc: 'title', //gives title for image
+    verticalFit: true, // Fits image in area vertically
+    tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+  }
+});
+
+//hut gallery
+$('#hut').magnificPopup({
+  items: [
+    { 
+      src: 'images/hut.jpg',
+      title:'hut from front',
+      class:'img-fluid'
+    },
+    { 
+      src: 'images/hut-2.jpg',
+      title:'hut from top',
+      class:'img-fluid'
+    },
+           
+  ],
+  gallery: {
+    enabled: true 
+  },
+  type: 'image', // this is default type
+  image: {
+    titleSrc: 'title', //gives title for image
+    verticalFit: true, // Fits image in area vertically
+    tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+  }
 });
 
 });
